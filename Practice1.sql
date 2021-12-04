429bd502891dbc24f6bf428c62589c270081850f
vevdokimenko <evdokimenko@hotmail.com>
2021-12-04 10:58:50 +0200

    Practice. Task1

--- Practice1.sql
+++ Practice1.sql
@@ -3,37 +3,47 @@
 -- Лотерея. Один участник может вытянуть только 1 билет
 -- -------------------------------
 
-create DATABASE lottery;
+CREATE
+    DATABASE lottery;
 
-use lottery;
+USE
+    lottery;
 
-CREATE TABLE tickets (
-    ticket_id INT AUTO_INCREMENT NOT NULL,
-    ticket_number INT NOT NULL,
+CREATE TABLE tickets
+(
+    ticket_id     INT AUTO_INCREMENT NOT NULL,
+    ticket_number INT                NOT NULL,
     PRIMARY KEY (ticket_id)
 );
 
-insert into tickets (ticket_number) 
-values(12345), (23456), (34567);
+INSERT INTO tickets (ticket_number)
+VALUES (12345),
+       (23456),
+       (34567);
 
-SELECT * FROM tickets;
+SELECT *
+FROM tickets;
 
-CREATE TABLE participants (
+CREATE TABLE participants
+(
     participant_id INT AUTO_INCREMENT NOT NULL,
-    fName VARCHAR(30) NOT NULL,
-    lName VARCHAR(30) NOT NULL,
-    phone VARCHAR(13) NOT NULL UNIQUE,
-    ticket_id INT NOT NULL UNIQUE,
+    fName          VARCHAR(30)        NOT NULL,
+    lName          VARCHAR(30)        NOT NULL,
+    phone          VARCHAR(13)        NOT NULL UNIQUE,
+    ticket_id      INT                NOT NULL UNIQUE,
     FOREIGN KEY (ticket_id)
         REFERENCES tickets (ticket_id),
     PRIMARY KEY (participant_id)
 );
 
-INSERT INTO participants (fName, lName, phone, ticket_id) 
-values 
-('Ivan', 'Ivanov', '+380501234567', 3), 
-('Petrov', 'Peter', '+380951234567', 1), 
-('Sidorov', 'Serhii', '+380671234567', 2);
+INSERT INTO participants (fName, lName, phone, ticket_id)
+VALUES ('Ivan', 'Ivanov', '+380501234567', 3),
+       ('Petrov', 'Peter', '+380951234567', 1),
+       ('Sidorov', 'Serhii', '+380671234567', 2);
+
+SELECT *
+FROM participants;
 
 -- ERROR!
-INSERT INTO participants (fName, lName, phone, ticket_id) values ('Ivan', 'Ivanov', '+380671234567', 2);
+INSERT INTO participants (fName, lName, phone, ticket_id)
+VALUES ('Ivan', 'Ivanov', '+380671234567', 2);
